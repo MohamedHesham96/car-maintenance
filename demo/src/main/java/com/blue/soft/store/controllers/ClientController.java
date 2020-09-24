bfc19095b4f17ad4b7d006bdefb765b4ee2a7138
MohamedHesham96 <mohamed.hesham.fci@gmail.com>
2020-09-24 16:12:26 +0200

    Update Client Controller

---
+++ demo/src/main/java/com/blue/soft/store/controllers/ClientController.java
@@ -0,0 +1,43 @@
+package com.blue.soft.store.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.blue.soft.store.entity.Client;
+import com.blue.soft.store.entity.Client;
+import com.blue.soft.store.service.clientService;
+
+@Controller
+public class ClientController {
+
+	@Autowired
+	private HttpSession httpSession;
+
+	@Autowired
+	clientService clientService;
+
+	@RequestMapping("clients-list")
+	public String showClientsList(Model theModel) {
+
+		theModel.addAttribute("client", new Client());
+		theModel.addAttribute("clientsList", clientService.getAllClients());
+
+		return "clients-list";
+	}
+
+	@RequestMapping("/search-for-client")
+	public String searchForClient(@ModelAttribute(name = "clientName") String theClientName, Model theModel) {
+
+		theModel.addAttribute("client", new Client());
+		System.out.println(clientService.searchForClient(theClientName).get(0).getName());
+		theModel.addAttribute("clientsList", clientService.searchForClient(theClientName));
+
+		return "clients-list";
+	}
+
+}
